42b28aa595e9dc76bd9c83c33ab9d791633ceab4
Kartik Sharma <kartik.sharma1607@outlook.com>
2019-02-03 19:21:55 +0530

    Updated Card Stack widget

--- youtube_bottom_sheet/lib/main.dart
+++ youtube_bottom_sheet/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.red,
